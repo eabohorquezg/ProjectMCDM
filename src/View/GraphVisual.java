2e18b9fd477adfabdeef36e1c5195ac4f0409376
amrondonp <amrondonp@gmail.com>
2016-11-12 10:36:57 -0500

    Some refractioring and fixign graph size

--- src/View/GraphVisual.java
+++ src/View/GraphVisual.java
@@ -10,23 +10,13 @@ import edu.uci.ics.jung.algorithms.layout.CircleLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.graph.DirectedGraph;
 import edu.uci.ics.jung.graph.DirectedSparseGraph;
-import edu.uci.ics.jung.graph.Graph;
-import edu.uci.ics.jung.graph.SparseGraph;
 import edu.uci.ics.jung.visualization.BasicVisualizationServer;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
-import edu.uci.ics.jung.visualization.renderers.Renderer;
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.HeadlessException;
 import java.awt.Paint;
-import java.awt.Stroke;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.Stack;
-import java.util.TreeSet;
 import javax.swing.JFrame;
 import org.apache.commons.collections15.Transformer;
 
@@ -39,27 +29,8 @@ public class GraphVisual extends JFrame {
     private Map<Alternative, List<Alternative>> graph;
     private DirectedGraph<String, String> g;
 
-    private Stack<Alternative> s = new Stack<>();
-    private TreeSet<Alternative> visited = new TreeSet<>();
-
-    private void dfs(Alternative node, boolean first) {
-        visited.add(node);
-        for (Alternative nei : graph.get(node)) {
-            if (!visited.contains(nei)) {
-                if (!first) {
-                    System.out.println(node.getName() + " -> " + nei.getName());
-                    g.addEdge(node.getName() + nei.getName(), node.getName(), nei.getName());
-                }
-                dfs(nei, first);
-            }
-        }
-        if (first) {
-            s.push(node);
-        }
-    }
-
     public GraphVisual(Map<Alternative, List<Alternative>> graph) {
-        super();
+        super("Solución PROMETHEE 2");
         this.graph = graph;
         showGraph();
     }
@@ -67,31 +38,15 @@ public class GraphVisual extends JFrame {
     public void showGraph() {
         g = new DirectedSparseGraph<>();
 
-        Alternative start = null;
         for (Alternative a : graph.keySet()) {
             g.addVertex(a.getName());
-            for(Alternative e : graph.get(a)){
-                g.addEdge(a.getName()+e.getName(), a.getName(),e.getName());
+            for (Alternative e : graph.get(a)) {
+                g.addEdge(a.getName() + e.getName(), a.getName(), e.getName());
             }
-            start = a;
         }
-        
-        
-        
-        /*dfs(start, true);
-        visited.clear();
 
-        while (!s.empty()) {
-            if (!visited.contains(s.peek())) {
-                dfs(s.peek(), false);
-            }
-            s.pop();
-        }*/
-
-        // The Layout<V, E> is parameterized by the vertex and edge types
         Layout<String, String> layout = new CircleLayout(g);
-        layout.setSize(new Dimension(360, 300)); // sets the initial size of the space
-        // The BasicVisualizationServer<V,E> is parameterized by the edge types
+        layout.setSize(new Dimension(370, 300));
 
         Transformer<String, Paint> vertexPaint = new Transformer<String, Paint>() {
             public Paint transform(String i) {
@@ -99,15 +54,13 @@ public class GraphVisual extends JFrame {
             }
         };
 
-        BasicVisualizationServer<String, String> vv
-                = new BasicVisualizationServer<>(layout);
+        BasicVisualizationServer<String, String> vv = new BasicVisualizationServer<>(layout);
         vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
         vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
-        vv.setPreferredSize(new Dimension(410, 350)); //Sets the viewing area size
+        vv.setPreferredSize(new Dimension(420, 350));
 
         this.setLocation(750, 0);
         this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         this.getContentPane().add(vv);
     }
-
 }
